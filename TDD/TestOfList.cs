56044f896c8aecbdfb876718d6d0abd1e4613d2e
Julia Holodiy <holodiyjulietta@gmail.com>
2017-03-22 00:06:06 +0300

    создание односвязного списка

---
+++ TDD/TestOfList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD
+{
+    [TestClass]
+    public class TestOfList
+    {
+        [TestMethod]
+        public void CreateLinkedList()
+        {
+            LinkedList list = new LinkedList();
+        }
+    }
+}
