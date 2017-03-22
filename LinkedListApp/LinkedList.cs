cfce7f130803c6087a9cc4360d52d054583b7aab
Julia Holodiy <holodiyjulietta@gmail.com>
2017-03-22 10:17:51 +0300

    создала метод Delete

--- LinkedListApp/LinkedList.cs
+++ LinkedListApp/LinkedList.cs
@@ -67,5 +67,45 @@ namespace LinkedListApp
             return false;
         }
 
+        public bool Delete(T data)
+        {
+            Node<T> current = head;
+            Node<T> previous = null;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    // Если узел в середине или в конце
+                    if (previous != null)
+                    {
+                        // убираем узел current, теперь previous ссылается не на current, а на current.Next
+                        previous.Next = current.Next;
+
+                        // Если current.Next не установлен, значит узел последний,
+                        // изменяем переменную tail
+                        if (current.Next == null)
+                            tail = previous;
+                    }
+                    else
+                    {
+                        // если удаляется первый элемент
+                        // переустанавливаем значение head
+                        head = head.Next;
+
+                        // если после удаления список пуст, сбрасываем tail
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
     }
 }
